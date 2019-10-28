dc1c0bee9d4ac3d7ca4b3d7d2420296e12c93aba
janmichek <jan@keboola.com>
2019-10-28 10:10:26 +0100

    add compiled components

---
+++ dist/components/autocomplete.vue
@@ -0,0 +1,11 @@
+<template>
+  <input
+    ref="input"
+    v-bind="$attrs"
+    v-on="$listeners"
+    />
+</template>
+
+<script>
+export default ((x) => x.default || x)(require('../components-implementation/autocomplete'))
+</script>
